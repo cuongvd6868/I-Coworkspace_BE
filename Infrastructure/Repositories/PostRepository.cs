e9b61ab30ee4c35451d17de328826cd32f64bd97
unknown <vuongdinhcuong.k@gmail.com>
2026-03-06 12:26:30 +0700

    post service

---
+++ Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PostRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Post>> GetAllPostsAsync()
+        {
+            return await _context.Posts
+                .ToListAsync();
+        }
+
+        public async Task<Post> GetPostByIdAsync(int id)
+        {
+            return await _context.Posts
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task AddPostAsync(Post post)
+        {
+            await _context.Posts.AddAsync(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdatePostAsync(Post post)
+        {
+            _context.Posts.Update(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePostAsync(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Post>> GetFeaturedPostsAsync()
+        {
+            return await _context.Posts
+                .Where(p => p.IsFeatured)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Infrastructure/Services/PostService.cs
@@ -0,0 +1,63 @@
+﻿using Application.DTOs.Post;
+using Application.Interfaces;
+using Application.Mappings;
+using Domain.Entities;
+using Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task<IEnumerable<PostResponseDto>> GetAllPostsAsync()
+        {
+            var entities = await _postRepository.GetAllPostsAsync();
+            return entities.Select(e => e.ToDto());
+        }
+
+        public async Task<IEnumerable<PostResponseDto>> GetFeaturedPostsAsync()
+        {
+            var entities = await _postRepository.GetFeaturedPostsAsync();
+            return entities.Select(e => e.ToDto());
+        }
+
+        public async Task<PostResponseDto> GetPostByIdAsync(int id)
+        {
+            var entity = await _postRepository.GetPostByIdAsync(id);
+            return entity.ToDto(); 
+        }
+
+        public async Task CreatePostAsync(Post post)
+        {
+            await _postRepository.AddPostAsync(post);
+        }
+
+        public async Task UpdatePostAsync(Post post)
+        {
+            var existing = await _postRepository.GetPostByIdAsync(post.Id);
+            if (existing == null) throw new KeyNotFoundException("Không tìm thấy bài đăng");
+
+            existing.Title = post.Title;
+            existing.ContentMarkdown = post.ContentMarkdown;
+            existing.ContentHtml = post.ContentHtml;
+            existing.ImageData = post.ImageData;
+            existing.IsFeatured = post.IsFeatured;
+
+            await _postRepository.UpdatePostAsync(existing);
+        }
+
+        public async Task DeletePostAsync(int id)
+        {
+            await _postRepository.DeletePostAsync(id);
+        }
+    }
+}
